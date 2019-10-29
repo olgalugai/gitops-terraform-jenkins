ae78d9cfe21dadc7f043620e602b498a2a840fe3
olgalugai <38784980+olgalugai@users.noreply.github.com>
2019-10-29 16:19:15 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,9 +21,6 @@ resource "aws_instance" "default" {
   source_dest_check      = false
   instance_type          = "${var.instance_type}"
 
-  tags {
-    Name = "terraform-default"
-  }
 }
 
 # Create Security Group for EC2
